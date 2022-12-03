7268abb64f9c9e362c25ac51d3631a2df0f9a705
Soldo <matteo.soldini@live.com>
2022-12-03 12:26:07 +0100

    ✏️ Add resource_cost_per_item

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import pandas as pd
+from IPython.display import display
+
 
 sales = pd.read_excel("files/Vendite.xlsx", decimal=',')
 customers = pd.read_excel("files/Clienti.xlsx", decimal=',')
 exchange_rates = pd.read_excel("files/Tassi di cambio.xlsx", decimal=',')
 
+# Budget sales
 budget_exchange_rates = exchange_rates.loc[exchange_rates['Anno'] == "BUDGET"]
 budget_sales = sales.loc[(sales['budget/cons'] == "BUDGET")]
 
@@ -14,8 +17,7 @@ budget_sales_with_exchange_rates = pd.merge(
 euro_budget_sales = budget_sales_with_exchange_rates['Importo vendita in valuta locale (TOTALE VENDITA)'] / \
     budget_sales_with_exchange_rates['Tasso di cambio medio']
 
-print(euro_budget_sales.sum())
-
+# Final sales
 final_exchange_rates = exchange_rates.loc[exchange_rates['Anno']
                                           == "CONSUNTIVO"]
 final_sales = sales.loc[(sales['budget/cons'] == "Consuntivo")]
@@ -27,15 +29,38 @@ final_sales_with_exchange_rates = pd.merge(
 euro_final_sales = final_sales_with_exchange_rates['Importo vendita in valuta locale (TOTALE VENDITA)'] / \
     final_sales_with_exchange_rates['Tasso di cambio medio']
 
-print(euro_final_sales.sum())
+# Budget resource
+# Production volume
+resource_usage = pd.read_excel(
+    "files/Impiego orario risorse.xlsx", decimal=',')
 
-# 'Importo vendita in valuta locale (TOTALE VENDITA)'].sum()
+budget_resource_usage = resource_usage.loc[resource_usage['budget/consuntivo'] == 'BUDGET']
+budget_production_volume = budget_resource_usage['Quantità di output'].sum()
 
-# print(budget_sales)
+# Total cost
+budget_resources_cost = pd.read_excel(
+    "files/Costo orario risorse - budget.xlsx", decimal=',')
 
-"""
-df = pd.merge(sales, customers, how="left",
-              left_on='Nr. origine', right_on='Nr.').head()
+budget_resource_usage_with_cost = budget_resource_usage.merge(budget_resources_cost, how="left", left_on=[
+                                                              'Risorsa', 'Nr. Area di produzione'], right_on=['Risorsa', 'Area di produzione'])
 
-print(df)
-"""
+budget_resource_usage_with_cost['Costo (€)'] = budget_resource_usage_with_cost['Tempo risorsa'] * \
+    budget_resource_usage_with_cost['Costo orario (€/h)']
+
+resource_cost_per_item = budget_resource_usage_with_cost.groupby(
+    by=["nr articolo"]).sum()
+
+print(resource_cost_per_item)
+
+# Final resource
+final_resource_usage = resource_usage.loc[resource_usage['budget/consuntivo'] == 'CONSUNTIVO']
+final_production_volume = final_resource_usage['Quantità di output'].sum()
+
+# creating a DataFrame
+dict = {'Context': ['Budget', 'Final'],
+        'Sales': [euro_budget_sales.sum(), euro_final_sales.sum()],
+        'Production Volume': [budget_production_volume, final_production_volume]}
+df = pd.DataFrame(dict)
+
+# displaying the DataFrame
+display(df)
